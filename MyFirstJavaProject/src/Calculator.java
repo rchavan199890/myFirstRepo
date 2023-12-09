73bcfc0a821dc9d20720cb7aa6b11a4247a3231d
ramchavan00001 <ramchavan00001@gmail.com>
2023-12-09 22:57:31 +0530

    Calculator full program

--- MyFirstJavaProject/src/Calculator.java
+++ MyFirstJavaProject/src/Calculator.java
@@ -6,10 +6,25 @@ public class Calculator {
 		System.out.println("Calculator\n");
 		Calculator cal=new Calculator();
 		System.out.println("The addition is: "+cal.addition(3, 6));
+		System.out.println("The substraction is: "+cal.substraction(3, 6));
+		System.out.println("The multiplication is: "+cal.multiplication(3, 6));
+		System.out.println("The division is: "+cal.division(3, 6));
 	}
 	public int addition(int a, int b)
 	{
 		return a+b;
 		
+	}
+	public int substraction(int a, int b)
+	{
+		return a-b;
+	}
+	public int multiplication(int a, int b)
+	{
+		return a*b;
+	}
+	public int division(int a, int b)
+	{
+		return a/b;
 	}
 }
